b3d29bf4948f18a205d7ff6d410a3f9c67538baf
Brendan Apple <brendancapple@gmail.com>
2025-09-07 20:41:49 -0400

    Added a new page for SCLaNT-JE

--- src/comp/header.js
+++ src/comp/header.js
@@ -10,7 +10,6 @@ class HeaderBar extends HTMLElement {
             <button class="BAR_BUTTON" onclick="window.location.href='index.html'">Home</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Art</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='code.html'">Code</button> |
-            <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Modeling</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Rocketry</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Video</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Resume</button> |
