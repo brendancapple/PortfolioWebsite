e13419a61ad3ce1084ec931cb2fc4d2f27e9c3cc
Brendan Apple <brendancapple@gmail.com>
2025-09-22 13:46:44 -0400

    Rocketry Page

--- src/comp/header.js
+++ src/comp/header.js
@@ -10,7 +10,7 @@ class HeaderBar extends HTMLElement {
             <button class="BAR_BUTTON" onclick="window.location.href='index.html'">Home</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Art</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='code.html'">Code</button> |
-            <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Rocketry</button> |
+            <button class="BAR_BUTTON" onclick="window.location.href='rocketry.html'">Rocketry</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Video</button> |
             <button class="BAR_BUTTON" onclick="window.location.href='art.html'">Resume</button> |
         </div>
